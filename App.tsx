6296c2f9cd95a936de9764912adae13f55f94a37
Paul Buscano <pbuscano@deloitte.com>
2022-05-17 11:31:25 +0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return <View></View>;
+};
+
+export default App;
